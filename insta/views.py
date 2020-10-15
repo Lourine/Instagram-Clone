6093d5dfd524e5982976b5152474aa2c23c41d99
Lourine <lourine.millicent@gmail.com>
2020-10-15 12:00:45 +0300

    Create Insta app with necessary modules

---
+++ insta/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def index(request):
+     return render(request, 'index.html')
+
+def signup_view(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('index')
+    return render(request, 'signup.html', {'form': form})
