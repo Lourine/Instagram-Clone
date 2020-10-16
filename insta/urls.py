d0e1cb831683c1a724f6ad917dd126673e7f1e3a
Lourine <lourine.millicent@gmail.com>
2020-10-16 03:37:00 +0300

    Add search url

--- insta/urls.py
+++ insta/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.index,name = 'index'),
+    url('',views.index,name = 'index'),
+    url('signup/', views.signup, name='signup'),
+    url('account/', include('django.contrib.auth.urls')),
+    url(r'^search/', views.search_results, name='search_results')
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
