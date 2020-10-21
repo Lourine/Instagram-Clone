ba6bde861ef456523c4258c16f3f68f5e23769f5
Lourine <lourine.millicent@gmail.com>
2020-10-21 12:01:50 +0300

    Add urls path

--- insta/urls.py
+++ insta/urls.py
@@ -1,15 +1,24 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from .views import (
+Home, ViewPost, CreatePost,
+UpdatePost,DeletePost, ViewProfile,
+CreateComment,DeleteComment,
+LikePostAPI, ViewLikes, ViewNotifications
+)
 
-urlpatterns=[
-    url('',views.index,name = 'index'),
-    url('signup/', views.signup, name='signup'),
-    url('account/', include('django.contrib.auth.urls')),
-    url(r'^search/', views.search_results, name='search_results')
 
+urlpatterns = [
+    path('', Home.as_view(), name='insta-home'),
+    path('search/', views.search, name='search'),
+    path('post/<int:pk>/', ViewPost.as_view(), name='insta-detail'),
+    path('post/new/', CreatePost.as_view(), name='insta-create'),
+    path('post/<int:pk>/update', UpdatePost.as_view(), name='insta-update'),
+    path('post/<int:pk>/delete/', DeletePost.as_view(), name='insta-delete'),
+    path('user/<str:username>/', ViewProfile.as_view(), name='insta-profile'),
+    path('post/<int:pk>/comment/', CreateComment.as_view(), name='insta-comment'),
+    path('comment/<int:pk>/delete/', DeleteComment.as_view(), name='insta-delete_comment'),
+    path('post/<int:pk>/like_api/', LikePostAPI.as_view(), name='insta-post_like_api'),
+    path('post/<int:pk>/likes/', ViewLikes.as_view(), name='insta-post_likes'),
+    path('notifications/', ViewNotifications.as_view(), name='insta-notifications'),
 ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
